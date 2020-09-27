548e3a3fd37d9877753980826a07da28e48a181a
Arthur Barré <arthur.barre@easy.movie>
2020-09-27 20:30:37 +0200

    mongo db for server

--- api/database.js
+++ api/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const DATABASE_CONNECTION = "mongodb://localhost:27017/mongo-test";
+const DATABASE_CONNECTION = "mongodb://mongo:27017/expressmongo";
 
 var PathSchema = mongoose.Schema({
   userId: Number,
