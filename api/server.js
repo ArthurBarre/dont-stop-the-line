952848e361f3c475c8c95b0557a60c18ec3b4d0e
Arthur Barré <arthur.barre@easy.movie>
2020-09-27 19:59:44 +0200

    add cors

--- api/server.js
+++ api/server.js
@@ -2,15 +2,20 @@ const express = require("express");
 var bodyParser = require("body-parser");
 const db = require("./database");
 const app = express();
-var cors = require('cors')
+var cors = require("cors");
 
 const apiPaths = require("./routes");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
 db.initializeMongo();
-
+app.applyMiddleware({
+  cors: {
+    origin: "http://127.0.0.1:5500/",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  },
+});
 app.get("/", function (req, res) {
   db.Path.find(function (err, paths) {
     if (err) return console.error(err);
